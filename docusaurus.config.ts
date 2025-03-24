5f99aa0c77532239537f5c7d980ed88b574cf6c4
antares-sw <23400824+antares-sw@users.noreply.github.com>
2025-03-24 19:09:57 +0300

    Fix build

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   organizationName: "stakewise", // Usually your GitHub org/user name.
   projectName: "docs", // Usually your repo name.
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set

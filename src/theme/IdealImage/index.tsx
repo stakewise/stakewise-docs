a1fd04d86dd90ccf23896f2920442096ccf5b01c
ulieth <uliana.eth@proton.me>
2026-03-23 10:44:52 +0300

    updated Boost feature

--- src/theme/IdealImage/index.tsx
+++ src/theme/IdealImage/index.tsx
@@ -1,13 +1,26 @@
 import React, { type ReactNode } from 'react'
 import OriginalIdealImage from '@theme-original/IdealImage'
 import type { Props } from '@theme/IdealImage'
+import { useColorMode } from '@docusaurus/theme-common'
 
+interface ThemedProps extends Props {
+  sources?: {
+    light: string
+    dark: string
+  }
+}
 
-const IdealImage = (props: Props): ReactNode => (
-  <div className="ideal-image-wrapper">
-    <OriginalIdealImage {...props} />
-  </div>
-)
+const IdealImage = ({ sources, ...props }: ThemedProps): ReactNode => {
+  const { colorMode } = useColorMode()
+  const img = sources
+    ? (colorMode === 'dark' ? sources.dark : sources.light)
+    : props.img
 
+  return (
+    <div className="ideal-image-wrapper">
+      <OriginalIdealImage key={sources ? colorMode : undefined} {...props} img={img} />
+    </div>
+  )
+}
 
 export default IdealImage

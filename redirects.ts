f88763419c3934d11e67bbce0f26c28c82f939c6
dfkadyr <dfkadyr@gmail.com>
2026-04-07 17:18:35 +0300

    add redirects plugin and configure redirects for documentation

---
+++ redirects.ts
@@ -0,0 +1,22 @@
+export default [
+  {
+    from: '/operator/intro',
+    to: '/operator/introduction',
+  },
+  {
+    from: '/operator/v3/intro',
+    to: '/operator/operator-service-v3/overview',
+  },
+  {
+    from: '/operator/additional-actions/fee-claiming',
+    to: '/operator/manage-vault/fee-claiming',
+  },
+  {
+    from: '/docs/guides/running-a-vault/how-to-create-a-vault',
+    to: '/operator/create-your-vault',
+  },
+  {
+    from: '/operator/additional-actions/process-metavaults',
+    to: '/operator/manage-vault/process-metavaults',
+  },
+]
